c2fef2aa116d988c7ed8bdece1bb6e1df4a923cc
Tran Hoang Duy <duytrieudong@gmail.com>
2023-07-30 21:53:19 +0700

    ket noi mongo

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import usersRouter from './routes/users.routes'
+import databaseService from './services/database.services'
 const app = express()
 const port = 3000
 app.use(express.json())
+//connect to database
+databaseService.connect()
 app.use('/users', usersRouter)
 
 app.listen(port, () => {
