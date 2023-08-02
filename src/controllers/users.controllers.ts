959793999026f18d9e8caca5931727fd3675260a
Tran Hoang Duy <duytrieudong@gmail.com>
2023-08-02 10:52:48 +0700

    tao accesstoken va refreshtoken

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express'
+import { ParamsDictionary } from 'express-serve-static-core'
+import { RegisterReqBody } from '~/models/requests/User.requests'
 import usersService from '~/services/users.services'
 export const loginController = (req: Request, res: Response) => {
   const { email, password } = req.body
@@ -11,12 +13,12 @@ export const loginController = (req: Request, res: Response) => {
     error: 'Login failed'
   })
 }
-export const registerController = async (req: Request, res: Response) => {
-  const { email, password } = req.body
+export const registerController = async (req: Request<ParamsDictionary, any, RegisterReqBody>, res: Response) => {
   try {
-    const result = await usersService.register({ email, password })
+    const result = await usersService.register(req.body)
     res.json({
-      message: 'Register successful'
+      message: 'Register successful',
+      result
     })
   } catch (error) {
     res.status(400).json({
---
+++ src/models/requests/User.requests.ts
@@ -0,0 +1,7 @@
+export interface RegisterReqBody {
+  name: string
+  email: string
+  password: string
+  body: string
+  date_of_birth: string
+}
