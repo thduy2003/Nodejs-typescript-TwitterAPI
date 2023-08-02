b487813dd9a5f991a7322c72ef7c3609e61ced22
Tran Hoang Duy <duytrieudong@gmail.com>
2023-08-02 23:01:10 +0700

    Khai bao message validation

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,3 +1,21 @@
 export const USERS_MESSAGES = {
-  VALIDATION_ERROR: 'Validation error'
+  VALIDATION_ERROR: 'Validation error',
+  NAME_IS_REQUIRED: 'Name is required',
+  NAME_MUST_BE_A_STRING: 'Name must be a string',
+  EMAIL_ALREADY_EXISTS: 'Email already exists',
+  NAME_LENGTH_MUST_BE_FROM_1_TO_100: 'Name must be must be from 1 to 100 characters',
+  EMAIL_IS_REQUIRED: 'Email is required',
+  EMAIL_IS_INVALID: 'Email is invalid',
+  PASSWORD_IS_REQUIRED: 'Password is required',
+  PASSWORD_MUST_BE_A_STRING: 'Password must be a string',
+  PASSWORD_LENGTH_MUST_BE_FROM_6_TO_50: 'Password must be from 6 to 50 characters',
+  PASSWORD_MUST_BE_STRONG:
+    'Password must be at least 6 characters long and contain at least lowercase letter, 1 uppercase letter, 1 number and 1 symbol',
+  CONFIRM_PASSWORD_IS_REQUIRED: 'Password is required',
+  CONFIRM_PASSWORD_MUST_BE_A_STRING: 'Password must be a string',
+  CONFIRM_PASSWORD_LENGTH_MUST_BE_FROM_6_TO_50: 'Password must be from 6 to 50 characters',
+  CONFIRM_PASSWORD_MUST_BE_STRONG:
+    'Password must be at least 6 characters long and contain at least lowercase letter, 1 uppercase letter, 1 number and 1 symbol',
+  CONFIRM_PASSWORD_MUST_BE_THE_SAME_AS_PASSWORD: 'Password confirm does not match password',
+  DATE_OF_BIRTH_MUST_BE_ISO8601: 'Date of birth must be ISO8601'
 } as const
