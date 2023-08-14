04bdf33816afd6369e3c40e12fdc2a57815c725e
Tran Hoang Duy <duytrieudong@gmail.com>
2023-08-14 22:59:16 +0700

    filter upload anh

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -1,19 +1,9 @@
 import { NextFunction, Request, Response } from 'express'
 import path from 'path'
+import { handleUploadSingleImage } from '~/utils/file'
 export const uploadSingleImageController = async (req: Request, res: Response, next: NextFunction) => {
-  const formidable = (await import('formidable')).default
-  const form = formidable({
-    uploadDir: path.resolve('uploads'),
-    keepExtensions: true,
-    maxFiles: 1,
-    maxFileSize: 300 * 1024
-  })
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      throw err
-    }
-    res.json({
-      message: 'Upload image successfully '
-    })
+  const data = await handleUploadSingleImage(req)
+  return res.json({
+    result: data
   })
 }
--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,3 +1,4 @@
+import { Request } from 'express'
 import fs from 'fs'
 import path from 'path'
 export const initFolder = () => {
@@ -7,4 +8,32 @@ export const initFolder = () => {
       recursive: true // mục đích là để tạo folder nested
     })
   }
+}
+export const handleUploadSingleImage = async (req: Request) => {
+  const formidable = (await import('formidable')).default
+  const form = formidable({
+    uploadDir: path.resolve('uploads'),
+    keepExtensions: true,
+    maxFiles: 1,
+    maxFileSize: 300 * 1024,
+    filter: function ({ name, originalFilename, mimetype }) {
+      const valid = name === 'image' && Boolean(mimetype?.includes('image/'))
+      if (!valid) {
+        form.emit('error' as any, new Error('File type is not valid') as any)
+      }
+      return valid
+    }
+  })
+  return new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return reject(err)
+      }
+      // eslint-disable-next-line no-extra-boolean-cast
+      if (!Boolean(files.image)) {
+        return reject(new Error('File is empty'))
+      }
+      resolve(files)
+    })
+  })
 }
