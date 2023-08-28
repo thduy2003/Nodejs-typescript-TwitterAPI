667003ba31cc5c028fd6f7c5f94dc2d3acb536d4
Tran Hoang Duy <duytrieudong@gmail.com>
2023-08-28 20:56:46 +0700

    fix bug lỗi liên quan key Aws

--- src/middlewares/error.middlewares.ts
+++ src/middlewares/error.middlewares.ts
@@ -4,15 +4,32 @@ import { omit } from 'lodash'
 import HTTP_STATUS from '~/constants/httpStatus'
 
 export const defaultErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof ErrorWithStatus) {
-    return res.status(err.status).json(omit(err, ['status']))
+  try {
+    if (err instanceof ErrorWithStatus) {
+      return res.status(err.status).json(omit(err, ['status']))
+    }
+    //để cho thằng err.message có thể lấy được string vì object Error nó không có method to JSON nên JSON.stringify(err) nhận từ Object Error ra empty object
+    // Object.getOwnPropertyNames(err).forEach((key) => {
+    //   Object.defineProperty(err, key, { enumerable: true })
+    // })
+    const finalError: any = {}
+    Object.getOwnPropertyNames(err).forEach((key) => {
+      if (
+        !Object.getOwnPropertyDescriptor(err, key)?.configurable ||
+        !Object.getOwnPropertyDescriptor(err, key)?.writable
+      ) {
+        return
+      }
+      finalError[key] = err[key]
+    })
+    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      error: err.message,
+      errorInfo: omit(err, ['stack'])
+    })
+  } catch (error) {
+    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      error: 'Internal Server Error',
+      errorInfo: omit(error as any, ['stack'])
+    })
   }
-  //để cho thằng err.message có thể lấy được string vì object Error nó không có method to JSON nên JSON.stringify(err) nhận từ Object Error ra empty object
-  Object.getOwnPropertyNames(err).forEach((key) => {
-    Object.defineProperty(err, key, { enumerable: true })
-  })
-  res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-    error: err.message,
-    errorInfo: omit(err, ['stack'])
-  })
 }
